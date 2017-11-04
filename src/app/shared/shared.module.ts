83273556dd05f8d3feeab13219b6ef8a09fb83f2
Abhinav Kumar <abby37kapoor@gmail.com>
2017-11-04 18:56:39 +0530

    navbar done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
-
+// import { AppComponent } from './../app.component';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     AppHeaderComponent,
