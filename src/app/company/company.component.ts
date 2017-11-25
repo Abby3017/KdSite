717cbf827cff92ec7d91ad6de7a63b058a2a38a1
Abhinav Kumar <abby37kapoor@gmail.com>
2017-11-25 18:42:26 +0530

    about us dummy page created

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import { ViewEncapsulation } from '@angular/core'
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
+  providers:[NgbCarouselConfig],
 })
 export class CompanyComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) { 
+    config.wrap = false;
+    // config.keyboard = false;
+    config.interval = 500;
+  }
 
   ngOnInit() {
   }
